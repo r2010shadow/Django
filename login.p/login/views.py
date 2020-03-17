362a476aaf5aeb3a5c358670e8481ddb968a96b5
r2010shadow <r2010shadow@163.com>
2020-03-17 14:33:28 +0800

    Captcha

--- login.p/login/views.py
+++ login.p/login/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from . import models
+from . import forms
 
 def index(request):
     pass
@@ -9,27 +10,34 @@ def index(request):
 def login(request):
 
     if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+        # username = request.POST.get('username')
+        # password = request.POST.get('password')
+
+        login_form = forms.UserForm(request.POST)
         message = 'Open Sesome ! '
-        if username.strip() and password:# 确保用户名和密码都不为空
+
+        if login_form.is_valid():     # if username.strip() and password:# 确保用户名和密码都不为空
+            username = login_form.cleaned_data.get('username')
+            password = login_form.cleaned_data.get('password')
             # 用户名字符合法性验证
             # 密码长度验证
             # 更多的其它验证.....
             try:        #验证用户名和密码
                 user = models.User.objects.get(name = username)
             except:
-                message = 'USERNAME in the air'
-                return render(request, 'login/login.html',{'message':message})
-            if user.password == password:
-                print('[CAUTION]: ', username, password)
+                message = 'USER in the air'
+                return render(request, 'login/login.html',locals())  #返回当前所有的本地变量字典 {'message':message})
+            if user.password == password:                            #也可能往模板传入了一些多余的变量数据，造成数据冗余降低效率
+                print('[Air]: ', username, password)
                 return redirect('/index/')
             else:
                 message = 'PASSWORD in the air!'
-                return render(request, 'login/login.html',{'message': message})
+                return render(request, 'login/login.html', locals()) #{'message': message})
         else:
-            return render(request, 'login/login.html', {'message': message})
-    return render(request, 'login/login.html')
+            return render(request, 'login/login.html', locals())  #{'message': message})
+
+    login_form = forms.UserForm()
+    return render(request, 'login/login.html', {'login_form':login_form})     # 或新增 locals()
 
 
 def register(request):
--- login.p/mysite/urls.py
+++ login.p/mysite/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from login import views
 
 urlpatterns = [
@@ -23,4 +23,5 @@ urlpatterns = [
     path('login/', views.login),
     path('register/', views.register),
     path('logout/', views.logout),
+    path('captcha/', include('captcha.urls'))
 ]
